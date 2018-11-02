e66704ee5f7dd2ad0a35d587f9c293629e0d230c
Jordan <jordanclark96@hotmail.com>
2018-11-02 15:30:58 +1300

    Unity Scene

---
+++ Steampunk Spacewhale/Assets/Test/Player/Gravity.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+
+    float gravity = -9.8f;
+    Rigidbody rb;
+
+    public Transform backLeft;
+    public Transform backRight;
+    public Transform frontLeft;
+    public Transform frontRight;
+
+    public RaycastHit lr;
+    public RaycastHit rr;
+    public RaycastHit lf;
+    public RaycastHit rf;
+    public Vector3 upDir;
+
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+
+        //Gravity
+        rb.AddForce(transform.up * gravity);
+
+        //Clinging to terrain
+
+
+	}
+}
