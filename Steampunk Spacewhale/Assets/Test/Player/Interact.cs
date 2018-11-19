d207a4ab6d49a871a52ea7642a5bc5698244e3eb
thedudxo <Isaacboon@gmail.com>
2018-11-19 15:30:54 +1300

    3:30

---
+++ Steampunk Spacewhale/Assets/Test/Player/Interact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour {
+
+    public float armLength = 2f;
+
+    RaycastHit Hit;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
