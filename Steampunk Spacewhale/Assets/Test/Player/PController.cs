e11f4bb3b364cc87d0cb8f5740b9fa01c449d477
Jordan <jordanclark96@hotmail.com>
2018-11-20 15:28:41 +1300

    Bla bla bla

--- Steampunk Spacewhale/Assets/Test/Player/PController.cs
+++ Steampunk Spacewhale/Assets/Test/Player/PController.cs
@@ -8,10 +8,11 @@ public class PController : MonoBehaviour {
     public float turnSpeed = 90;
     public float lerpSpeed = 10; // smoothing speed
     public float gravity = 10;// gravity acceleration
-    public bool isGrounded;
     public float deltaGround = 0.2f; // character is grounded up to this distance
     public float jumpSpeed = 5; // vertical jump initial speed
-    public float jumpRange = 3; // range to detect target wall
+    public float jumpRange = 1.5f; // range to detect target wall
+    public bool isGrounded;
+    public GameObject currentFloor;
     private Rigidbody rb;
     private Vector3 surfaceNormal; // current surface normal
     private Vector3 myNormal; // character normal
@@ -27,14 +28,35 @@ public class PController : MonoBehaviour {
         }
     }
     private void Awake() {
-        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.lockState = CursorLockMode.Locked;
     }
-
+    
     void Start() {
         myNormal = transform.up; // normal starts as character up direction 
         gameObject.GetComponent<Rigidbody>().freezeRotation = true; // disable physics rotation
         // distance from transform.position to ground
         distGround = GetComponent<Collider>().bounds.extents.y - GetComponent<Collider>().bounds.center.y;
+    }
+    
+    private void OnCollisionEnter(Collision col) {
+        /*Ray colRay;
+        RaycastHit colHit;
+        colRay = new Ray(transform.position, -transform.up);
+        if (Physics.Raycast(colRay, out colHit, jumpRange)) {
+            currentFloor = colHit.collider.gameObject;
+        } if(col.collider.gameObject == currentFloor) {   
+            return;
+        } else { */
+            ContactPoint contact = col.contacts[0];
+        if (col.gameObject != currentFloor)
+        {
+            StartCoroutine(JumpToWall(contact.point, contact.normal));
+            Debug.Log("Switch");
+        } else
+        {
+            currentFloor = col.gameObject;
+        }
+        //}
     }
 
     void FixedUpdate() {
@@ -46,10 +68,10 @@ public class PController : MonoBehaviour {
         Ray ray;
         RaycastHit hit;
         if (Input.GetButtonDown("Jump")) { // jump pressed:
-            ray = new Ray(transform.position, transform.forward);
+            /*ray = new Ray(transform.position, transform.forward);
             if (Physics.Raycast(ray, out hit, jumpRange)) { // wall ahead?
                 StartCoroutine(JumpToWall(hit.point, hit.normal)); // yes: jump to the wall
-            } else if (isGrounded) { // no: if grounded, jump up
+            } else */if (isGrounded) { // no: if grounded, jump up
                 rb.velocity += jumpSpeed * myNormal;
                 isGrounded = false;
             }
@@ -57,17 +79,17 @@ public class PController : MonoBehaviour {
 
         // update surface normal and isGrounded:
         ray = new Ray(transform.position, -myNormal); // cast ray downwards
-        if (Physics.Raycast(ray, out hit, 5f)) { // use it to update myNormal and isGrounded
+        if (Physics.Raycast(ray, out hit, jumpRange)) { // use it to update myNormal and isGrounded
             isGrounded = hit.distance <= distGround + deltaGround;
             surfaceNormal = hit.normal;
             isGrounded = true;
         } else {
             isGrounded = false;
             // assume usual ground normal to avoid "falling forever"
-            surfaceNormal = Vector3.up;
+            //surfaceNormal = Vector3.up;
         }
         Debug.DrawRay(transform.position, transform.forward * jumpRange);
-        Debug.DrawRay(transform.position, -transform.up * Mathf.Infinity, Color.red);
+        Debug.DrawRay(transform.position, -transform.up * jumpRange, Color.red);
     }
 
     void Update() {
@@ -78,7 +100,7 @@ public class PController : MonoBehaviour {
         // align character to the new myNormal while keeping the forward direction:
         var targetRot = Quaternion.LookRotation(myForward, myNormal);
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, lerpSpeed * Time.deltaTime);
-        // move the character forth/back with Vertical axis:
+        // move the character
         transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
         //Debug.Log(moveSpeed);
     }
@@ -94,7 +116,7 @@ public class PController : MonoBehaviour {
         var dstRot = Quaternion.LookRotation(myForward, normal);
         myNormal = normal; // update myNormal
         for (float t = 0.0f; t < 1.0;) {
-            t += Time.deltaTime;
+            t += Time.deltaTime * 2;
             transform.position = Vector3.Lerp(orgPos, dstPos, t);
             transform.rotation = Quaternion.Slerp(orgRot, dstRot, t);
             yield return t;  // return here next frame
