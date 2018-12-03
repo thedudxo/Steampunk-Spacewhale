2fdd11cfaed0caf281d213b22e01cd2095d0df53
Jordan <jordanclark96@hotmail.com>
2018-12-04 12:31:21 +1300

    Redo

---
+++ Steampunk Spacewhale/Assets/Project/Player/Footsteps.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour {
+
+	void Update () {
+		if (PController.Instance.isGrounded && Input.GetAxis("Horizontal") != 0 && Input.GetAxis("Vertical") != 0 && GetComponent<AudioSource>().isPlaying == false) {
+            GetComponent<AudioSource>().volume = Random.Range(0.8f, 1);
+            GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.1f);
+            GetComponent<AudioSource>().Play();
+        }
+	}
+}
